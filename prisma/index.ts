2b45705ff036a457305f8ff6e66bc96b6dad0e50
Ansh Kumar <anshkumar8710@gmail.com>
2026-02-03 17:09:05 +0530

    Added prisma adapter

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from '@/prisma/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-
 import { env } from '@/configs/env';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({
   connectionString: env.DATABASE_URL
